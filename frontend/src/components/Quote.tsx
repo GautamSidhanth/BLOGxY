6bb5316be3435f2095be8fcbc51ccca26232c430
Gautam Sidhanth <gautamsidhanth14@gmail.com>
2024-11-30 18:01:25 +0530

    first commit

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "Focus on Career Because At the End People Don't see your Heart, They see your Success"
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Gautam Sidhanth
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    STUDENT | VIT BHOPAL
+                </div>
+            </div>
+        </div>
+        
+    </div>
+};
